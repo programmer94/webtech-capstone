7115ad9a78907a32afa7daf7b4b66600d920cd9f
Michael Gutterman <mgutterman@sbcglobal.net>
2017-12-09 20:51:16 -0500

    Added directories for stylesheets and scripts. Added index.html, main.css, and normalize.css. Changed tags to lowercase for consistency in NewBookstoreEntry.php.

--- NewBookstoreEntry.php
+++ NewBookstoreEntry.php
@@ -1,37 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <link rel="stylesheet" href="styles/main.css">
+        <link rel="stylesheet" type="text/css" href="styles/normalize.css">
+        <link rel="stylesheet" type="text/css" href="styles/main.css">
         <meta charset="utf-8">
         <title>New Book Entry</title>
     </head>
     <body>
         <h1>New Book Entry</h1>
         <form id="newEntry" action="newEntryActionPage.php" method="post">
-            <TABLE>
-                <TR>
-                    <TD>ISBN&nbsp</TD>
-                    <TD><input type="text" class="inputText" id="ISBN" name="ISBN"></TD>
-                </TR>
-                <TR>
-                    <TD>Title&nbsp</TD>
-                    <TD><input type="text" class="inputText" id="Title" name="Title"></TD>
-                </TR>
-                <TR>
-                    <TD>Publisher ID&nbsp</TD>
-                    <TD><input type="text" class="inputText" id="PID" name="PID"></TD>
-                    <TD>
+            <table>
+                <tr>
+                    <td>ISBN&nbsp</td>
+                    <td><input type="text" class="inputText" id="ISBN" name="ISBN"></td>
+                </tr>
+                <tr>
+                    <td>Title&nbsp</td>
+                    <td><input type="text" class="inputText" id="Title" name="Title"></td>
+                </tr>
+                <tr>
+                    <td>Publisher ID&nbsp</td>
+                    <td><input type="text" class="inputText" id="PID" name="PID"></td>
+                    <td>
                         <button type="button" id="searchPublishers">Search or Add Publishers
-                    </TD>
-                </TR>
-                <TR>
-                    <TD>Author ID&nbsp</TD>
-                    <TD><input type="text" class="inputText" id="AID" name="AID"></TD>
-                    <TD>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Author ID&nbsp</td>
+                    <td><input type="text" class="inputText" id="AID" name="AID"></td>
+                    <td>
                         <button type="button" id="searchAuthors">Search or Add Authors
-                    </TD>
-                </TR>
-            </TABLE>
+                    </td>
+                </tr>
+            </table>
         </form>
     </body>
 </html>
