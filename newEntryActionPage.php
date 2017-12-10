3f1d31e54b3db39e6d3a1c73b6e9f33124550530
jtufts <jtufts@umich.edu>
2017-12-10 11:54:54 -0500

    Variable Initialization
    Initialized the connection and variables for use in insertion queries related to the new book entry function.

--- newEntryActionPage.php
+++ newEntryActionPage.php
@@ -1,3 +1,23 @@
 <?php
+  $con = mysqli_connect('localhost','nesrin','p4@cis435','bookstore','3306');
+  
+  $isbn = "'";
+  $isbn .= $_POST['ISBN'];
+  $isbn .= "'";
+    
+  $title = "'";
+  $title .= $_POST['TITLE'];
+  $title .= "'";
 
+  $pid = "'";
+  $pid .= $_POST['PID'];
+  $pid .= "'";
+
+  $aid = "'";
+  $aid .= $_POST['AID'];
+  $aid .= "'";
+
+  $bid = "'";
+  $bid .= $_POST['BID'];
+  $bid .= "'";
 ?>
