6b723923078474b49b19e839c87f0f66ff9fa1a0
jtufts <jtufts@umich.edu>
2017-12-10 16:47:19 -0500

    Add Supplier Functionality
    Similar functionality to Add Book functionality

---
+++ newSupplierEntryActionPage.php
@@ -0,0 +1,40 @@
+
+<?php
+
+  $con = mysqli_connect('localhost','nesrin','p4@cis435','bookstore','3306');
+
+  $sid = "'";
+
+  $sid .= $_POST['SID'];
+
+  $sid .= "'";
+
+
+
+  $name = "'";
+
+  $name .= $_POST['name'];
+
+  $name .= "'";
+
+  $add = "'";
+  
+  $add .= $_POST['add'];
+  
+  $add .= "'";
+  
+  $phone = "'";
+  
+  $phone .= $_POST['phone'];
+  
+  $phone .= "'";
+
+
+  $query = "INSERT INTO book (SupplierID, Name, Address, Phone_Num) VALUES ($sid, $name, $add, $phone)";
+
+
+  $result = mysqli_query($con, $query);
+
+  header("Location: Table_Author.php");
+
+?>
