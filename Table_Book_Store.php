72f1a81d5454d7e68e282bd04e723265a70e6aca
namohamm <34376834+namohamm@users.noreply.github.com>
2017-12-10 11:15:57 -0500

    pages to show tables.
    the pages should be called from index.

---
+++ Table_Book_Store.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<!-- CIS435.F17: Final Project -->
+<!-- BookStore_DB.Book_Store _Table -->
+<html>
+   <head>
+      <meta charset = "utf-8">
+      <title>Book Store</title>
+      <style type = "text/css">
+         table  { background-color: lightblue;
+                  border: 1px solid gray;
+                  border-collapse: collapse; }
+         th, td { padding: 5px; border: 1px solid gray; }
+         tr:nth-child(even) { background-color: white; }
+         tr:first-child { background-color: lightgreen; }
+      </style>
+   </head>
+   <body>
+      <?php
+      //php 7
+	print( "<p><a href = 'index.php'>Home</a></p>");
+         // build SELECT query
+         $query = "SELECT * FROM book_store";
+
+         // Connect to MySQL
+         if ( !( $database = mysqli_connect('localhost','nesrin','p4@cis435','bookstore','3306') ) )
+            die( "<p>Could not connect to datbase</p></body></html>" );
+
+         // open database
+         if ( !mysqli_select_db( $database, "bookstore" ) )
+            die( "<p>Could not open bookstore database</p>
+               </body></html>" );
+
+         // query database
+         if ( !( $result = mysqli_query( $database, $query ) ) )
+         {
+            print( "<p>Could not execute query!</p>" );
+            die( mysqli_error() . "</body></html>" );
+         } // end if
+      ?><!-- end PHP script -->
+
+      <h1>Book Store DB</h1>
+      <table>         
+         <tr>
+            <th>Store Number</th>
+            <th>Address</th>
+            <th>Phone Number</th>           			
+         </tr>
+         <?php
+            // fetch each record in result set
+            for ( $counter = 0; $row = mysqli_fetch_row( $result ); ++$counter )
+            {
+               // build table to display results
+               print( "<tr>" );
+
+               foreach ( $row as $key => $value )
+                  print( "<td>$value</td>" );
+
+               print( "</tr>" );
+            } // end for
+
+            mysqli_close( $database );
+         ?><!-- end PHP script -->
+      </table>
+   </body>
+</html>
---
+++ Table_Employee.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<!-- CIS435.F17: Final Project -->
+<!-- BookStore_DB.Employee_Table -->
+<html>
+   <head>
+      <meta charset = "utf-8">
+      <title>Book Store</title>
+      <style type = "text/css">
+         table  { background-color: lightblue;
+                  border: 1px solid gray;
+                  border-collapse: collapse; }
+         th, td { padding: 5px; border: 1px solid gray; }
+         tr:nth-child(even) { background-color: white; }
+         tr:first-child { background-color: lightgreen; }
+      </style>
+   </head>
+   <body>
+      <?php
+      //php 7
+	print( "<p><a href = 'index.php'>Home</a></p>");
+         // build SELECT query
+         $query = "SELECT * FROM employee";
+
+         // Connect to MySQL
+         if ( !( $database = mysqli_connect('localhost','nesrin','p4@cis435','bookstore','3306') ) )
+            die( "<p>Could not connect to datbase</p></body></html>" );
+
+         // open database
+         if ( !mysqli_select_db( $database, "bookstore" ) )
+            die( "<p>Could not open bookstore database</p>
+               </body></html>" );
+
+         // query database
+         if ( !( $result = mysqli_query( $database, $query ) ) )
+         {
+            print( "<p>Could not execute query!</p>" );
+            die( mysqli_error() . "</body></html>" );
+         } // end if
+      ?><!-- end PHP script -->
+
+      <h1>Employee DB</h1>
+      <table>         
+         <tr>
+            <th>Employee ID</th>
+            <th>Name</th>
+            <th>Address</th>
+            <th>Phone Number</th>
+            <th>Hired Date</th>
+            <th>Pay Rate</th>
+            <th>Position</th>
+         </tr>
+         <?php
+            // fetch each record in result set
+            for ( $counter = 0; $row = mysqli_fetch_row( $result ); ++$counter )
+            {
+               // build table to display results
+               print( "<tr>" );
+
+               foreach ( $row as $key => $value )
+                  print( "<td>$value</td>" );
+
+               print( "</tr>" );
+            } // end for
+
+            mysqli_close( $database );
+         ?><!-- end PHP script -->
+      </table>
+   </body>
+</html>
