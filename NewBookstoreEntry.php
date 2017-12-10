b5f848e4e9c7a505f6941d1cd96a87ddb3786538
jtufts <jtufts@umich.edu>
2017-12-10 11:59:57 -0500

    Added Endings to Buttons and Added Bookshelf
    Re-added ending codes to each button to make sure the code doesn't blow up, and added bookshelf functionality

--- NewBookstoreEntry.php
+++ NewBookstoreEntry.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -18,27 +20,32 @@
         <form id="newEntry" action="newEntryActionPage.php" method="post">
             <table>
                 <tr>
-                    <td>ISBN&nbsp</td>
+                    <td>ISBN&nbsp </td>
                     <td><input type="text" class="inputText" id="ISBN" name="ISBN"></td>
                 </tr>
                 <tr>
-                    <td>Title&nbsp</td>
+                    <td>Title&nbsp </td>
                     <td><input type="text" class="inputText" id="Title" name="Title"></td>
                 </tr>
                 <tr>
-                    <td>Publisher ID&nbsp</td>
+                    <td>Publisher ID&nbsp </td>
                     <td><input type="text" class="inputText" id="PID" name="PID"></td>
                     <td>
-                        <button type="button" id="searchPublishers">Search or Add Publishers
+                        <button type="button" id="searchPublishers">Search or Add Publishers</button>
                     </td>
                 </tr>
                 <tr>
-                    <td>Author ID&nbsp</td>
+                    <td>Author ID&nbsp </td>
                     <td><input type="text" class="inputText" id="AID" name="AID"></td>
                     <td>
-                        <button type="button" id="searchAuthors">Search or Add Authors
+                        <button type="button" id="searchAuthors">Search or Add Authors</button>
                     </td>
                 </tr>
+                <tr>
+                    <td>Bookshelf Number&nbsp </td>
+                    <td><input type="text" class="inputText id="BID" name="BID"></td>
+                    <td><button type="button id="searchBookshelves">Search or Add Booksheves</button></td>
+            </tr>
             </table>
         </form>
     </body>
