50843f6e1c08fb7d769b2d8b2b4f95edffed8101
jtufts <jtufts@umich.edu>
2017-12-10 18:09:45 -0500

    Add Order Functionality
    Similar to Add Book Functionality

---
+++ NewOrderEntry.php
@@ -0,0 +1,97 @@
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+    <head>
+
+        <link rel="stylesheet" type="text/css" href="styles/normalize.css">
+
+        <link rel="stylesheet" type="text/css" href="styles/main.css">
+
+        <meta charset="utf-8">
+
+        <title>New Order Entry</title>
+
+        <script type="text/javascript">
+
+            function returnToHomePage()
+
+            {
+
+                window.location.replace("index.html");
+
+            }
+
+        </script>
+
+    </head>
+
+    <body>
+
+        <div id="nav">
+
+			<ul>
+
+				<li><a href="index.html"><b>Book Store</b></a></li>
+
+				<li><a href="index.html"><b>Home</b></a></li>
+
+				<li class="active"><a href="NewBookstoreEntry.php">Add Book</a></li>
+
+				<li><a href="indexToTestTableCall.php">Book List</a></li>
+
+			</ul> 
+
+		</div>
+
+        <form id="newOrderEntry" action="newOrderEntryActionPage.php" method="post">
+
+            <table>
+
+                <tr>
+
+                    <td>Order ID&nbsp </td>
+
+                    <td><input type="text" class="inputText" id="OID" name="OID"></td>
+
+                </tr>
+
+                <tr>
+
+                    <td>Order Date&nbsp </td>
+
+                    <td><input type="text" class="inputText" id="oDate" name="oDate"></td>
+
+                </tr>
+				
+				<tr>
+
+                    <td>Order Time&nbsp </td>
+
+                    <td><input type="text" class="inputText" id="oTime" name="oTime"></td>
+
+                </tr>
+				
+				<tr>
+
+                    <td>Supplier ID&nbsp </td>
+
+                    <td><input type="text" class="inputText" id="SID" name="SID"></td>
+
+                </tr>
+				
+				<tr>
+
+                    <td>Employee ID&nbsp </td>
+
+                    <td><input type="text" class="inputText" id="EID" name="EID"></td>
+
+                </tr>
+            </table>
+
+        </form>
+
+    </body>
+
+</html>
---
+++ newOrderEntryActionPage.php
@@ -0,0 +1,43 @@
+
+<?php
+
+  $con = mysqli_connect('localhost','nesrin','p4@cis435','bookstore','3306');
+
+  $OID = "'";
+  
+  $OID .= $_POST['OID'];
+  
+  $OID .= "'";
+  
+  $oDate = "'";
+  
+  $oDate .= $_POST['oDate'];
+  
+  $oDate .= "'";
+  
+  $oTime = "'";
+  
+  $oTime .= $_POST['oTime'];
+  
+  $oTime .= "'";
+  
+  $SID = "'";
+  
+  $SID .= $_POST['SID'];
+  
+  $SID .= "'";
+  
+  $EID = "'";
+
+  $EID .= $_POST['EID'];
+
+  $EID .= "'";
+  
+
+  $query = "INSERT INTO publisher (Order_ID, OrderDate, OrderTime, SupplierID, EmployeeID) VALUES ($OID, $oDate, $oTime, $SID, $EID)";
+
+  $result = mysqli_query($con, $query);
+
+  header("Location: Table_Order.php");
+
+?>
