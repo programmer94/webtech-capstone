e9c3edc1ae7f6db79d5cc9efd0864176dc8af45c
jtufts <jtufts@umich.edu>
2017-12-10 20:15:40 -0500

    Add Order Button
    Added a button that redirects the user to the add order page

--- Table_Order.php
+++ Table_Order.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <!-- CIS435.F17: Final Project -->
 <!-- BookStore_DB.Order_Table -->
@@ -15,6 +16,12 @@
       </style>
 		<link rel="stylesheet" type="text/css" href="styles/normalize.css">
 		<link rel="stylesheet" type="text/css" href="styles/main.css">
+      <script type="text/javascript">
+	      function redirectToAddOrder()
+	      {
+		      window.location.replace("NewOrderEntry.php");
+	      }
+     </script>
    </head>
    <body>
 		<div id="nav">			
@@ -46,7 +53,7 @@
             die( mysqli_error($database)."</body></html>");
          } // end if
       ?><!-- end PHP script -->
-
+	<button type="button" OnClick=redirectToAddOrder()>Add Order</button>
       <h1>Order DB</h1>
       <table>
         <tr>
