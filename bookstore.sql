9979e94ec5aac98c201952e00a428f8beb0ad47c
namohamm <34376834+namohamm@users.noreply.github.com>
2017-12-10 19:38:51 -0500

    SQL file: Author & Book tables are updated

---
+++ bookstore.sql
@@ -0,0 +1,216 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 11, 2017 at 01:36 AM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 7.1.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bookstore`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `author`
+--
+
+CREATE TABLE `author` (
+  `AuthorID` varchar(4) NOT NULL,
+  `firstName` varchar(25) NOT NULL,
+  `lastName` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `billing`
+--
+
+CREATE TABLE `billing` (
+  `InvoiceNumb` mediumint(9) NOT NULL,
+  `BillingDate` date DEFAULT NULL,
+  `Price` mediumint(9) DEFAULT NULL,
+  `Quantity` mediumint(9) DEFAULT NULL,
+  `ISBN` varchar(255) DEFAULT NULL,
+  `StoreNum` mediumint(9) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book`
+--
+
+CREATE TABLE `book` (
+  `ISBN` char(13) NOT NULL,
+  `Bookshelf_Num` char(4) DEFAULT NULL,
+  `PublisherID` char(4) DEFAULT NULL,
+  `Title` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book_shelf`
+--
+
+CREATE TABLE `book_shelf` (
+  `Bookshelf_Num` varchar(3) NOT NULL,
+  `Category` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book_store`
+--
+
+CREATE TABLE `book_store` (
+  `StoreNum` varchar(4) NOT NULL,
+  `Address` varchar(35) NOT NULL,
+  `PhoneNum` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `EmployeeID` int(4) NOT NULL,
+  `Name` varchar(20) DEFAULT NULL,
+  `Address` varchar(25) DEFAULT NULL,
+  `PhoneNum` char(13) DEFAULT NULL,
+  `HiredDate` date DEFAULT NULL,
+  `Pay_Rate` decimal(10,2) DEFAULT NULL,
+  `Position` varbinary(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inventory`
+--
+
+CREATE TABLE `inventory` (
+  `ISBN` varchar(13) NOT NULL,
+  `Quantity` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `OrderID` int(4) NOT NULL,
+  `OrderDate` date DEFAULT NULL,
+  `OrderTime` varchar(6) DEFAULT NULL,
+  `SupplierID` int(4) DEFAULT NULL,
+  `EmployeeID` int(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `publisher`
+--
+
+CREATE TABLE `publisher` (
+  `PublisherID` varchar(4) NOT NULL,
+  `Name` varchar(20) NOT NULL,
+  `Contact` varchar(20) NOT NULL,
+  `PhoneNum` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supplier`
+--
+
+CREATE TABLE `supplier` (
+  `SupplierID` int(4) NOT NULL,
+  `Name` varchar(30) NOT NULL,
+  `Address` varchar(35) NOT NULL,
+  `PhoneNum` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `billing`
+--
+ALTER TABLE `billing`
+  ADD PRIMARY KEY (`InvoiceNumb`);
+
+--
+-- Indexes for table `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`ISBN`);
+
+--
+-- Indexes for table `book_shelf`
+--
+ALTER TABLE `book_shelf`
+  ADD PRIMARY KEY (`Bookshelf_Num`);
+
+--
+-- Indexes for table `book_store`
+--
+ALTER TABLE `book_store`
+  ADD PRIMARY KEY (`StoreNum`);
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`EmployeeID`);
+
+--
+-- Indexes for table `inventory`
+--
+ALTER TABLE `inventory`
+  ADD PRIMARY KEY (`ISBN`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`OrderID`);
+
+--
+-- Indexes for table `publisher`
+--
+ALTER TABLE `publisher`
+  ADD PRIMARY KEY (`PublisherID`);
+
+--
+-- Indexes for table `supplier`
+--
+ALTER TABLE `supplier`
+  ADD PRIMARY KEY (`SupplierID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
